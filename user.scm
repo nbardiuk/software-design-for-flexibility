aa2b8371b653377765c520b4d5e26a1affa48f3c
Nazarii Bardiuk <nazarii@bardiuk.com>
2021-03-12 19:41:13 +0000

    exercise 2.2

--- user.scm
+++ user.scm
@@ -93,5 +93,96 @@
 
 ; Exercise 2.2: Arity extension
 
+(define arity-table
+  (make-key-weak-eqv-hash-table))
 
+(define (get-arity proc)
+  (or (hash-table-ref/default arity-table proc #f)
+      (procedure-arity proc)))
+
+(define (restrict-arity proc arity)
+  (hash-table-set! arity-table proc arity)
+  proc)
+
+(define (compose f g)
+  (let ((f-arity (get-arity f))
+        (g-arity (get-arity g)))
+    (guarantee-procedure-of-arity f (make-procedure-arity 1) 'f)
+    (define (the-composition . args)
+      (guarantee-procedure-of-arity g (make-procedure-arity (length args)) 'g)
+      (f (apply g args)))
+    (restrict-arity the-composition g-arity)))
+
+((compose
+   (lambda (u) (list 'bar u))
+   (lambda (x y z) (list 'foo x y z))) 'a 'b 'c)
+
+(get-arity (compose
+             (lambda (u) (list 'bar u))
+             (lambda (x y z) (list 'foo x y z))))
+
+(define (intersect-arities a b)
+  (let ((min-arity (max (procedure-arity-min a)
+                        (procedure-arity-min b)))
+        (max-arity (or (and (procedure-arity-max a)
+                            (procedure-arity-max b)
+                            (min (procedure-arity-max a)
+                                 (procedure-arity-max b)))
+                       (procedure-arity-max a)
+                       (procedure-arity-max b))))
+    (and max-arity (<= min-arity max-arity)
+         (make-procedure-arity min-arity max-arity))))
+
+(define (parallel-combine h f g)
+  (guarantee-procedure-of-arity h (make-procedure-arity 2) 'h)
+  (let ((common-arity (intersect-arities (get-arity f) (get-arity g))))
+    (assert common-arity)
+    (define (the-combination . args)
+      (let ((args-arity (make-procedure-arity (length args))))
+        (guarantee-procedure-of-arity f args-arity 'f)
+        (guarantee-procedure-of-arity g args-arity 'g)
+        (h (apply f args) (apply g args))))
+    (restrict-arity the-combination common-arity)))
+
+
+((parallel-combine
+   list
+   (lambda (x y z) (list 'foo x y z))
+   (lambda (u v w) (list 'bar u v w)))
+ 'a 'b 'c)
+
+(get-arity (parallel-combine
+    list
+    list
+    (lambda (u v w) (list 'bar u v w))))
+
+(define (sum-arities a b)
+  (let ((min-arity (+ (procedure-arity-min a)
+                      (procedure-arity-min b)))
+        (max-arity (and (procedure-arity-max a)
+                        (procedure-arity-max b)
+                        (+ (procedure-arity-max a)
+                           (procedure-arity-max b)))))
+    (make-procedure-arity min-arity max-arity)))
+
+(define (spread-combine h f g)
+  (let ((n (procedure-arity-min (get-arity f)))
+        (m (procedure-arity-min (get-arity g)))
+        (common-arity (sum-arities (get-arity f) (get-arity g))))
+    (guarantee-procedure-of-arity h (make-procedure-arity 2) 'h)
+    (let ((t (+ n m))) ;; todo there should be some constraint solving here, this prevents a lot of valid applications
+      (define (the-combination . args)
+        (assert (= (length args) t))
+        (h (apply f (list-head args n))
+           (apply g (list-tail args n))))
+      (restrict-arity the-combination common-arity))))
+
+((spread-combine list
+                 (lambda (x y) (list 'foo x y))
+                 (lambda (u v w) (list 'bar u v w)))
+ 'a 'b 'c 'd 'e)
+
+(get-arity (spread-combine list
+                 (lambda (x y) (list 'foo x y))
+                 (lambda (u v w) (list 'bar u v w))))
 ;; (RESTART 1)
